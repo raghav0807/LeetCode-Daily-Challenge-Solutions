65bf98f45cebd98bb2a593a2d84ff274ab4de788
raghav0807 <raghav.agiwal2002@gmail.com>
2024-09-12 17:49:57 +0530

    1684. Count the Number of Consistent Strings(E)- 12 Sep 2024

---
+++ 1684. Count the Number of Consistent Strings(E)- 12 Sep 2024.java
@@ -0,0 +1,28 @@
+class Solution {
+  public int countConsistentStrings(String allowed, String[] words) {
+      boolean[] isAllowed = new boolean[26];
+      
+      for (char c : allowed.toCharArray()) {
+          isAllowed[c - 'a'] = true;
+      }
+      
+      int count = 0;
+      
+      for (String word : words) {
+          boolean isConsistent = true;
+          
+          for (char c : word.toCharArray()) {
+              if (!isAllowed[c - 'a']) {
+                  isConsistent = false;
+                  break;
+              }
+          }
+          
+          if (isConsistent) {
+              count++;
+          }
+      }
+      
+      return count;
+  }
+}
